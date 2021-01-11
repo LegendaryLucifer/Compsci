f1782c0f0d753758d1d70fb2c4299201d5d85821
LegendaryLucifer <43181865+LegendaryLucifer@users.noreply.github.com>
2021-01-11 12:14:29 -0600

    Add files via upload

---
+++ Filmst.java
@@ -0,0 +1,35 @@
+// Filmst.java
+// The Array of Film Objects program
+// This is the student, starting version of the Ch12Film assignment.
+public class Filmst
+{
+public static void main(String [] args)
+{
+// Create an array called watch that contains a list of 4 film objects
+// Initialize the values of each object in the array.
+watch[0] = new Film("Shrek",133,"PG");
+watch[1] = new Film("Road to Perdition",117,"G");
+watch[2] = new Film("The Truth about Cats and Dogs",93,"PG");
+watch[3] = new Film("Enigma",114,"G");
+// loop to increase the length time of each object in the array by 10%
+// loop to display the film information of each object in the array.
+}
+}
+class Film{
+String title;
+int length;
+String rating; // "G", "PG", "PG-13", "R"
+public Film (String sname, int min, String r)
+{
+}
+public void display()
+{
+}
+public int getLength()
+{
+return 0;
+}
+public void setLength(int len) //len corresponds with the percentage the film is to be increased by
+{
+}
+}
